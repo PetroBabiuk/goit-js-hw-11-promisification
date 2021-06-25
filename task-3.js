b934780605696868b1805d26fcc67c8133bfa442
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-06-25 09:43:09 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -14,15 +14,15 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
       
       if (canProcess) {
-      resolve(transaction.id, delay);
-      } else {
+        resolve({ id: transaction.id, time: delay });
+      } 
       reject(transaction.id);
-      }
+      
     }, delay);  
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
